e14d821c118da475f57f21096afbaf88ba2daee3
RodriMont <rodrigo.montero@itsrizzoli.it>
2026-08-05 17:17:27 +0200

    recap

---
+++ ballastasera/src/main/java/com/kryptosystems/ballastasera/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.kryptosystems.ballastasera.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    /*@Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.GET, "/api/health").permitAll()
+                        .anyRequest().authenticated())
+                .oauth2Login(oauth2 -> {});
+        return http.build();
+    }*/
+}
